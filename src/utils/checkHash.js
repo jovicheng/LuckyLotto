00ad66bb3dd86bfb2d79c542c84c682290444fb1
jovicheng <jovicheng@tencent.com>
2019-12-05 20:01:21 +0800

    更换测试网合约地址

--- src/utils/checkHash.js
+++ src/utils/checkHash.js
@@ -1,17 +1,18 @@
 import axios from 'axios'
 
-export default function checkHash(hashOptions) {
+export default function checkHash (hashOptions) {
   axios({
-      method: 'post',
-      url: 'https://mainnet.nebulas.io/v1/user/getTransactionReceipt',
-      data: {
-        "hash": hashOptions.txhash
-      }
-    }).then(function (response) {
+    method: 'post',
+    url: 'https://testnet.nebulas.io/v1/user/getTransactionReceipt',
+    data: {
+      hash: hashOptions.txhash
+    }
+  })
+    .then(function (response) {
       hashOptions.successFunction(response)
     })
     .catch(function (error) {
       hashOptions.errorFunction(error)
-      console.log(error);
-    });
+      console.log(error)
+    })
 }
