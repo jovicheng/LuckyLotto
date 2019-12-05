a83ff204d75ebba57dafcad547fd4b0eab462be7
jovicheng <jovicheng@tencent.com>
2019-12-05 19:52:35 +0800

    更换测试网合约地址

--- src/utils/nebRequest.js
+++ src/utils/nebRequest.js
@@ -1,34 +1,26 @@
-export default function nebRequest(options) {
+export default function nebRequest (options) {
   // var dappAddress = 'n1zT4n7MzWCxKWzpQJ5yVEqDEVt6xez6VKu'
   var dappAddress = 'n1jo9c839go3k1SKXLaLpXUo3oX6wARpMyH' // 测试网
   // 主网地址
-  var nebulas = require('nebulas'),
-    Account = nebulas.Account,
-    neb = new nebulas.Neb()
-  neb.setRequest(new nebulas.HttpRequest("https://mainnet.nebulas.io")); //主网链接
-  // neb.setRequest(new nebulas.HttpRequest('https://testnet.nebulas.io')) //测试网链接
+  var nebulas = require('nebulas')
+
+  var Account = nebulas.Account
+
+  var neb = new nebulas.Neb()
+  // neb.setRequest(new nebulas.HttpRequest("https://mainnet.nebulas.io")); //主网链接
+  neb.setRequest(new nebulas.HttpRequest('https://testnet.nebulas.io')) // 测试网链接
   var NebPay = require('nebpay.js')
   var nebPay = new NebPay()
   //   var options = {
   //     callback: "https://pay.nebulas.io/api/testnet/pay" //可以指定交易查询服务器
   //   };
   if (options.call) {
-    nebPay.call(
-      dappAddress,
-      options.value,
-      options.callFunction,
-      options.callArgs, {
-        listener: options.successFunc
-      }
-    )
+    nebPay.call(dappAddress, options.value, options.callFunction, options.callArgs, {
+      listener: options.successFunc
+    })
   } else {
-    nebPay.simulateCall(
-      dappAddress,
-      options.value,
-      options.callFunction,
-      options.callArgs, {
-        listener: options.successFunc
-      }
-    )
+    nebPay.simulateCall(dappAddress, options.value, options.callFunction, options.callArgs, {
+      listener: options.successFunc
+    })
   }
 }
